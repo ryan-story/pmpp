cf860f34e0a69cf5e435aeab9d55a4593decae97
Piotr Mazurek <piotr635@gmail.com>
2024-05-05 18:39:41 +0200

    Chapter 2 (#1)
    * init commit with chapter two questions
    
    * Minor changes to answers organization
    
    * answer to 2E3
    
    * wip
    
    * vector multiplication from PyTorch added
    
    * Readme chapter two update

---
+++ chapter-2/code/vecMul.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+import torch
+from torch.utils.cpp_extension import load_inline
+from time import time
+
+
+def vector_multipication_loop(A: torch.Tensor, B: torch.Tensor) -> torch.Tensor:
+    assert A.device == B.device and A.device.type == "cuda"
+    assert A.dtype == B.dtype == torch.float32
+    assert A.size() == B.size()
+
+    C = torch.empty_like(A)
+    n = A.size(0)
+    for i in range(n):
+        C[i] = A[i] * B[i]
+    return C
+
+def compile_extension():
+    cuda_source = (Path(__file__).parent / "vecMulTorchTensor.cu").read_text()
+    cpp_source = "torch::Tensor vector_multiplication(torch::Tensor A, torch::Tensor B_h);"
+
+    extension = load_inline(
+        name="extension",
+        cpp_sources=cpp_source,
+        cuda_sources=cuda_source,
+        functions=["vector_multiplication"],
+        with_cuda=True,
+        # extra_cuda_cflags=["-O2"]
+    )
+
+    return extension
+
+def main():
+    ext = compile_extension()
+
+    DEVICE = "cuda"
+    DTYPE = torch.float32
+    NUM_ELEMENTS = 500_000
+
+    A = torch.tensor([i for i in range(NUM_ELEMENTS)]).to(DEVICE, DTYPE)
+    B = torch.tensor([i for i in range(NUM_ELEMENTS)]).to(DEVICE, DTYPE)
+
+    start = time()
+    y_custom_kernel = ext.vector_multiplication(A, B)
+    stop = time()
+    print(f"Cuda custom kernel multiply: {stop - start:.2f}s")
+    
+    start = time()
+    y_loop = vector_multipication_loop(A, B)
+    stop = time()
+    print(f"Python loop: {stop - start:.2f}s")
+    
+    start = time()
+    y_pytorch = A + B
+    stop = time()
+    print(f"Adding via PyTorch addition: {stop - start:.2f}s")
+
+
+    print("Size:", y_custom_kernel.size())
+    print("Y:", y_custom_kernel[:10])
+
+if __name__ == "__main__":
+    main()
---
+++ chapter-2/code/vecMulTorchTensor.cu
@@ -0,0 +1,29 @@
+#include <c10/cuda/CUDAException.h>
+#include <c10/cuda/CUDAStream.h>
+
+__global__
+void vecMulKernel(float* A, float* B, float* C, int n){
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n){
+        C[i] = A[i] * B[i];
+    }
+}
+
+torch::Tensor vector_multiplication(torch::Tensor A, torch::Tensor B){
+    assert(A.device().type() == torch::kCUDA && B.device().type() == torch::kCUDA);
+    assert(A.dtype() == torch::kFloat32 && B.dtype() == torch::kFloat32);
+    assert(A.size(0) == B.size(0));
+
+    int n = A.size(0);
+    auto C = torch::empty({n}, torch::TensorOptions().dtype(torch::kFloat32).device(A.device()));
+
+    // // Number of threads and blocks
+    int threads_per_block = 256;
+    int number_of_blocks = (n + threads_per_block - 1) / threads_per_block;
+
+    vecMulKernel<<<number_of_blocks, threads_per_block,  0, torch::cuda::getCurrentCUDAStream()>>>(A.data_ptr<float>(), B.data_ptr<float>(), C.data_ptr<float>(), n);
+
+    C10_CUDA_KERNEL_LAUNCH_CHECK();
+
+    return A;
+}
