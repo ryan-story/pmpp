b765d772466c49be48b8a5c71e8b4539917991f0
Piotr Mazurek <piotr635@gmail.com>
2024-12-20 20:32:03 +0000

    Made it compile again

--- chapter-7/code/conv2d_functions.cu
+++ chapter-7/code/conv2d_functions.cu
@@ -35,3 +35,22 @@ __global__ void conv2d_kernel_with_constant_memory(float *M, float *P, int r, in
     if ((outRow < height) && (outCol < width))
         P[outRow*width + outCol] = Pvalue;
 }
+
+void conv2d_with_constant_memory(float *M, float *F, float *P, int r, int height, int width){
+
+    float *d_M, *d_P;
+
+    cudaMalloc((void**)&d_M, width * height * sizeof(float));
+    cudaMalloc((void**)&d_P, width * height *sizeof(float));
+
+    cudaMemcpy(d_M, M, width * height * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_P, M, width * height * sizeof(float), cudaMemcpyHostToDevice);
+
+    cudaMemcpyToSymbol(constFilter, F, (2*r+1) * (2*r+1) * sizeof(float));
+
+    dim3 dimBloc(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 dimGrid(cdiv(width, dimBloc.x), cdiv(height, dimBloc.y));
+
+    conv2d_kernel_with_constant_memory<<<dimGrid, dimBloc>>>(d_M, d_P, r, height, width);
+    cudaMemcpy(P, d_P, width * height *sizeof(float), cudaMemcpyDeviceToHost);
+}
--- chapter-7/code/conv2d_functions.cuh
+++ chapter-7/code/conv2d_functions.cuh
@@ -18,4 +18,6 @@ inline unsigned int cdiv(unsigned int a, unsigned int b) {
 __global__ void conv2d_kernel(float *M, float *F, float *P, int r, int height, int width);
 __global__ void conv2d_kernel_with_constant_memory(float *M, float *P, int r, int height, int width);
 
+void conv2d_with_constant_memory(float *M, float *F, float *P, int r, int height, int width);
+
 #endif
