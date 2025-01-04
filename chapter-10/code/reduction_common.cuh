d8c948362dee688a260ca2e3f86763cd7627b3c5
Piotr Mazurek <piotr635@gmail.com>
2025-01-04 07:32:42 +0000

    Move the common testing functions into a seperate cuda file so they can be resused accross different programs

---
+++ chapter-10/code/reduction_common.cuh
@@ -0,0 +1,29 @@
+#pragma once
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <assert.h>
+
+// Error checking macros
+#define CUDA_CHECK(call) \
+do { \
+    cudaError_t error = call; \
+    if (error != cudaSuccess) { \
+        fprintf(stderr, "CUDA error at %s:%d: %s\n", \
+            __FILE__, __LINE__, \
+            cudaGetErrorString(error)); \
+        exit(EXIT_FAILURE); \
+    } \
+} while(0)
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+
+// Common function declarations
+void clear_l2();
+bool is_close(float a, float b, float rtol = 1e-5, float atol = 1e-8);
+float benchmark_sum_reduction(float (*func)(float *, int),
+                            float *data, unsigned int length,
+                            int warmup = 25, int reps = 100);
+float sequential_sum_reduction(float *data, int length);
--- chapter-10/code/reduction_sum.cu
+++ chapter-10/code/reduction_sum.cu
@@ -1,84 +1,8 @@
-// nvcc reduction_sum.cu -o reduction_sum
-#include <cuda_runtime.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-#include <assert.h>
+// nvcc reduction_sum.cu reduction_common.cu -o reduction_sum
+#include "reduction_common.cuh"
 
 #define BLOCK_DIM 1024
 #define COARSE_FACTOR 2
-
-#define CUDA_CHECK(call) \
-do { \
-    cudaError_t error = call; \
-    if (error != cudaSuccess) { \
-        fprintf(stderr, "CUDA error at %s:%d: %s\n", \
-            __FILE__, __LINE__, \
-            cudaGetErrorString(error)); \
-        exit(EXIT_FAILURE); \
-    } \
-} while(0)
-
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
-{
-    if (code != cudaSuccess)
-    {
-        fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-        if (abort) exit(code);
-    }
-}
-
-void clear_l2() {
-    static int l2_clear_size = 0;
-    static unsigned char* gpu_scratch_l2_clear = NULL;
-    if (!gpu_scratch_l2_clear) {
-        cudaDeviceGetAttribute(&l2_clear_size, cudaDevAttrL2CacheSize, 0);
-        l2_clear_size *= 2;
-        gpuErrchk(cudaMalloc(&gpu_scratch_l2_clear, l2_clear_size));
-    }
-    gpuErrchk(cudaMemset(gpu_scratch_l2_clear, 0, l2_clear_size));
-}
-
-bool is_close(float a, float b, float rtol = 1e-5, float atol = 1e-8) {
-    float diff = fabs(a - b);
-    float tolerance = atol + rtol * fabs(b);
-    return diff <= tolerance;
-}
-
-float benchmark_sum_reduction(float (*func)(float *, int),
-                            float *data, unsigned int length,
-                            int warmup = 25, int reps = 100){
-
-    for (int i = 0; i < warmup; ++i){
-        func(data, length);
-    }
-
-    cudaEvent_t iterStart, iterStop;
-    cudaEventCreate(&iterStart);
-    cudaEventCreate(&iterStop);
-
-    float totalTime_ms = 0.0f;
-
-    for(int i = 0; i < reps; ++i){
-        clear_l2();
-        cudaEventRecord(iterStart);
-        func(data, length);
-        cudaEventRecord(iterStop);
-        cudaEventSynchronize(iterStop);
-
-        float iterTime = 0.0f;
-        cudaEventElapsedTime(&iterTime, iterStart, iterStop);
-        totalTime_ms += iterTime;
-    }
-
-    cudaEventDestroy(iterStart);
-    cudaEventDestroy(iterStop);
-
-    return totalTime_ms / reps;
-}
-
 
 __global__ void segmented_sum_reduction_kernel(float* input, float* output, int length){
     __shared__ float input_s[BLOCK_DIM];
@@ -181,14 +105,6 @@ float coarsed_sum_reduction(float *data, int length){
     CUDA_CHECK(cudaFree(d_total));
 
     return total;
-}
-
-float sequential_sum_reduction(float *data, int length){
-    double total = 0.0;
-    for(unsigned int i = 0; i < length; ++i){
-        total += data[i];
-    }
-    return (float)total;
 }
 
 int main(int argc, char const *argv[]) {
